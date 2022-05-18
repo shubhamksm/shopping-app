7886a86b47acbffb5e2940e3c92c334ee9886688
Shubham Khandelwal <shukhand2@publicisgroupe.net>
2022-05-18 11:46:49 +0530

    Added axios base setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import React, { useEffect, useState } from "react";
+import apiClient from "./api";
+
 function App() {
+  const [data, setData] = useState(null);
+
+  const getAllData = async () => {
+    const resp = await apiClient.get("/banners");
+    console.log(resp);
+  };
+
+  useEffect(() => {
+    getAllData();
+  }, []);
+
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
